cb3c3ef358f862468c8a7c1cf94df75aaf2201ae
Md Noble <mdnoble.wd@gmail.com>
2023-11-27 14:25:31 +0600

    created reusable productCard

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { FaArrowAltCircleUp } from "react-icons/fa";
+
+const ProductCard = ({ item }) => {
+ 
+    const { image, name, tag, vote } = item || {};
+
+
+    return (
+        <div>
+            <div className="w-80 mx-auto bg-gray-100 p-1 rounded-lg border border-[#13a0fe]">
+              <img
+                className="rounded-lg rounded-br-[100px]"
+                src={image}
+                alt=""
+              />
+              <div className="my-4 h-20">
+                <h2 className="font-bold text-lg text-black mb-2">
+                  {name}
+                </h2>
+                <p className="font-semibold text-sm text-gray-500 bg-sky-300 w-fit rounded px-2 mx-auto">
+                  #{tag}
+                </p>
+              </div>
+              <button className="btn btn-outline w-10/12 text-[#13a0fe] mt-2 mb-4">
+                <FaArrowAltCircleUp className="text-4xl"></FaArrowAltCircleUp>
+                <span className="font-semibold text-lg ml-4">{vote}</span>
+              </button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
-import { FaArrowAltCircleUp } from "react-icons/fa";
+// import { FaArrowAltCircleUp } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle";
 import ButtonSky from "../../../components/ButtonSky";
+import ProductCard from "../../../components/ProductCard";
 
 const Trending = ({ items }) => {
   return (
@@ -12,27 +13,7 @@ const Trending = ({ items }) => {
       ></SectionTitle>
       <div className="text-center container mx-auto my-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {items.slice(0, 6).map((item) => (
-          <div key={item._id}>
-            <div className="w-80 mx-auto bg-gray-100 p-1 rounded-lg border border-[#13a0fe]">
-              <img
-                className="rounded-lg rounded-br-[100px]"
-                src={item.image}
-                alt=""
-              />
-              <div className="my-4 h-20">
-                <h2 className="font-bold text-lg text-black mb-2">
-                  {item.name}
-                </h2>
-                <p className="font-semibold text-sm text-gray-500 bg-sky-300 w-fit rounded px-2 mx-auto">
-                  #{item.tag}
-                </p>
-              </div>
-              <button className="btn btn-outline w-10/12 text-[#13a0fe] mt-2 mb-4">
-                <FaArrowAltCircleUp className="text-4xl"></FaArrowAltCircleUp>
-                <span className="font-semibold text-lg ml-4">{item.vote}</span>
-              </button>
-            </div>
-          </div>
+          <ProductCard key={item._id} item={item}></ProductCard>
         ))}
       </div>
       <ButtonSky buttonTitle={`Show all products`}></ButtonSky>
