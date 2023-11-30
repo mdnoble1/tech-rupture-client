8cff8170e7ddc77632e71e084a182311377fa830
Md Noble <mdnoble.wd@gmail.com>
2023-11-30 18:30:57 +0600

    added routes for admin in dashboard

--- src/pages/dashboard/Shared/Sidebar/Sidebar.jsx
+++ src/pages/dashboard/Shared/Sidebar/Sidebar.jsx
@@ -4,8 +4,12 @@ import { RiFileList3Fill } from "react-icons/ri";
 import { ImBoxAdd } from "react-icons/im";
 import logo from "../../../../assets/logo1.png";
 import { CgProfile } from "react-icons/cg";
+import { BsFillFileEarmarkBarGraphFill } from "react-icons/bs";
+import { RiCoupon2Fill } from "react-icons/ri";
 
 const Sidebar = () => {
+  const isAdmin = true;
+
   return (
     <div className="md:w-52 lg:w-96 bg-[#13a0fe] flex justify-center h-[1000px] min-h-full">
       <ul className="mt-6 lg:mt-12">
@@ -21,45 +25,92 @@ const Sidebar = () => {
             DASHBOARD
           </p>
         </div>
-        <li className="mb-6">
-          <NavLink
-            to="/dashboard/profile"
-            className={({ isActive }) =>
-              isActive
-                ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
-                : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
-            }
-          >
-            <CgProfile className="lg:text-3xl"></CgProfile>MY Profile
-          </NavLink>
-        </li>
-        <li className="mb-6">
-          <NavLink
-            to="/dashboard/addProduct"
-            className={({ isActive }) =>
-              isActive
-                ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
-                : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
-            }
-          >
-            <ImBoxAdd className="lg:text-2xl"> </ImBoxAdd> Add Product
-          </NavLink>
-        </li>
-        <li className="mb-10">
-          <NavLink
-            to="/dashboard/myProduct"
-            className={({ isActive }) =>
-              isActive
-                ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
-                : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
-            }
-          >
-            <RiFileList3Fill className="lg:text-3xl"></RiFileList3Fill> My
-            Product
-          </NavLink>
-        </li>
+        {isAdmin ? (
+          <div>
+            <li className="mb-6">
+              <NavLink
+                to="/dashboard/profile"
+                className={({ isActive }) =>
+                  isActive
+                    ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                }
+              >
+                <BsFillFileEarmarkBarGraphFill className="lg:text-3xl"></BsFillFileEarmarkBarGraphFill>{" "}
+                Statistics
+              </NavLink>
+            </li>
+            <li className="mb-6">
+              <NavLink
+                to="/dashboard/addProduct"
+                className={({ isActive }) =>
+                  isActive
+                    ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                }
+              >
+                <CgProfile className="lg:text-3xl"></CgProfile> Manage Users
+              </NavLink>
+            </li>
+            <li className="mb-10">
+              <NavLink
+                to="/dashboard/myProduct"
+                className={({ isActive }) =>
+                  isActive
+                    ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                }
+              >
+                <RiCoupon2Fill className="lg:text-3xl"></RiCoupon2Fill> Manage
+                Coupons
+              </NavLink>
+            </li>
+          </div>
+        ) : (
+          <div>
+            <li className="mb-6">
+              <NavLink
+                to="/dashboard/profile"
+                className={({ isActive }) =>
+                  isActive
+                    ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                }
+              >
+                <CgProfile className="lg:text-3xl"></CgProfile>MY Profile
+              </NavLink>
+            </li>
+            <li className="mb-6">
+              <NavLink
+                to="/dashboard/addProduct"
+                className={({ isActive }) =>
+                  isActive
+                    ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                }
+              >
+                <ImBoxAdd className="lg:text-2xl"> </ImBoxAdd> Add Product
+              </NavLink>
+            </li>
+            <li className="mb-10">
+              <NavLink
+                to="/dashboard/myProduct"
+                className={({ isActive }) =>
+                  isActive
+                    ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                }
+              >
+                <RiFileList3Fill className="lg:text-3xl"></RiFileList3Fill> My
+                Product
+              </NavLink>
+            </li>
+          </div>
+        )}
 
         <div className="border text-white w-full mb-10"></div>
+
+        {/* shared  */}
         <li className="mb-6">
           <NavLink
             to="/"
--- src/pages/dashboard/Shared/Topbar/Topbar.jsx
+++ src/pages/dashboard/Shared/Topbar/Topbar.jsx
@@ -5,8 +5,12 @@ import { CgProfile } from "react-icons/cg";
 import { ImBoxAdd } from "react-icons/im";
 import { RiFileList3Fill } from "react-icons/ri";
 import { FaHome } from "react-icons/fa";
+import { BsFillFileEarmarkBarGraphFill } from "react-icons/bs";
+import { RiCoupon2Fill } from "react-icons/ri";
 
 const Topbar = () => {
+  const isAdmin = true;
+
   return (
     <div className="navbar bg-[#13a0fe] p-4">
       <div className="navbar-start">
@@ -19,44 +23,90 @@ const Topbar = () => {
             <HiMenuAlt1 />
           </div>
           <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 border-2 border-[#13a0fe] rounded-box w-52">
-            <li className="mb-2">
-              <NavLink
-                to="/dashboard/userHome"
-                className={({ isActive }) =>
-                  isActive
-                    ? `text-[#13a0fe] flex items-center gap-3 font-bold uppercase`
-                    : `text-black flex items-center gap-3 font-bold uppercase`
-                }
-              >
-                <CgProfile className="text-2xl"></CgProfile>MY Profile
-              </NavLink>
-            </li>
-            <li className="mb-2">
-              <NavLink
-                to="/dashboard/reservation"
-                className={({ isActive }) =>
-                  isActive
-                    ? `text-[#13a0fe] flex items-center gap-3 font-bold uppercase`
-                    : `text-black flex items-center gap-3 font-bold uppercase`
-                }
-              >
-                <ImBoxAdd className="text-xl"> </ImBoxAdd> Add Product
-              </NavLink>
-            </li>
-            <li className="mb-4">
-              <NavLink
-                to="/dashboard/myProduct"
-                className={({ isActive }) =>
-                  isActive
-                    ? `text-[#13a0fe] flex items-center gap-3 font-bold uppercase`
-                    : `text-black flex items-center gap-3 font-bold uppercase`
-                }
-              >
-                <RiFileList3Fill className="text-2xl"></RiFileList3Fill> My
-                Product
-              </NavLink>
-            </li>
+            {isAdmin ? (
+              <div>
+                <li className="mb-6">
+                  <NavLink
+                    to="/dashboard/profile"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                        : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    }
+                  >
+                    <BsFillFileEarmarkBarGraphFill className="lg:text-3xl"></BsFillFileEarmarkBarGraphFill>{" "}
+                    Statistics
+                  </NavLink>
+                </li>
+                <li className="mb-6">
+                  <NavLink
+                    to="/dashboard/addProduct"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                        : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    }
+                  >
+                    <CgProfile className="lg:text-3xl"></CgProfile> Manage Users
+                  </NavLink>
+                </li>
+                <li className="mb-10">
+                  <NavLink
+                    to="/dashboard/myProduct"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                        : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    }
+                  >
+                    <RiCoupon2Fill className="lg:text-3xl"></RiCoupon2Fill>{" "}
+                    Manage Coupons
+                  </NavLink>
+                </li>
+              </div>
+            ) : (
+              <div>
+                <li className="mb-6">
+                  <NavLink
+                    to="/dashboard/profile"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                        : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    }
+                  >
+                    <CgProfile className="lg:text-3xl"></CgProfile>MY Profile
+                  </NavLink>
+                </li>
+                <li className="mb-6">
+                  <NavLink
+                    to="/dashboard/addProduct"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                        : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    }
+                  >
+                    <ImBoxAdd className="lg:text-2xl"> </ImBoxAdd> Add Product
+                  </NavLink>
+                </li>
+                <li className="mb-10">
+                  <NavLink
+                    to="/dashboard/myProduct"
+                    className={({ isActive }) =>
+                      isActive
+                        ? `text-white flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                        : `text-black flex items-center gap-3 font-bold cinzel text-xs lg:text-xl uppercase`
+                    }
+                  >
+                    <RiFileList3Fill className="lg:text-3xl"></RiFileList3Fill>{" "}
+                    My Product
+                  </NavLink>
+                </li>
+              </div>
+            )}
             <div className="border border-[#13a0fe] w-11/12 mx-auto mb-4"></div>
+            {/* shared  */}
             <li className="mb-6">
               <NavLink
                 to="/"
